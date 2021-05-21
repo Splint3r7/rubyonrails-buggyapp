19e115aa730c37033164d0fcfd8df1d37caa9e56
hassan.khan@sendoso.com <hassankhan14044@outlook.com>
2021-05-21 11:17:46 +0500

    Updates

--- app/controllers/example14_controller.rb
+++ app/controllers/example14_controller.rb
@@ -1,10 +1,23 @@
+MODELS = {
+  'product' => 'product',
+  'user' => 'user',
+  'users' => 'users'
+}
+
+
 class Example14Controller < ApplicationController
-	def testing_controller
+	#def testing_controller
 		#@dell_var = Post.where('id = ?', params[:id].constantize)
 		#@test = @dell_var.delete_all
-		@test = params[:alert][:type].constantize.new(params[:alert][:value])
+		#@test = params[:alert][:type].constantize.new(params[:alert][:value])
+	#	@test = params[:a].constantize.new(params[:b])
 		#alert[type]=test&alert[value]=test
 		#Checking if /etc/passwd exits
 		#http://127.0.0.1:3000/example14?alert[type]=File&alert[value]=/etc/passwd
+	#end
+
+## fix constantize model enumeration.
+	def testing_controller
+		MODELS.fetch(params[:a]
 	end
 end
